4acbc1562cb0ea8a2a7bd072670a8b54b82f6219
Mineaworld <mineawebdev@gmail.com>
2024-06-11 14:48:02 +0700

    Fixed import hero error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import Hero from "./components/Hero/Hero.jsx";
+import Hero from "./Components/Hero/Hero.jsx";
 import { ThemeProvider } from "./Common/ThemeContext.jsx";
 ThemeProvider;
 
